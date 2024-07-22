1c4cd9dd317da5110d486c1b782759c970648f6e
Uday Fating <123112024+Uday-Fating@users.noreply.github.com>
2024-07-22 11:17:00 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,20 @@
+# DjangoProject
+
+# Step1: - install virtual env using command pip install virtualenv
+# Step2: - activate virtual env using command .env/Scripts/activate
+# Step3: - install django using command pip install django
+# Step4: - create project using command django-admin startproject {project name }
+# Step5: - enter the project file env using command cd {project name}
+# Step6: - start the server using command py manage.py runserver (8001 to run on specific port or usually runs on 8000 by default)
+# Step7: - create a file views.py in folder where urls.py is present
+# Step8: - also create templates folder in the same level as manage.py in templates we could add html files
+# Step8: - also create static folder in the same level as manage.py in static folder we add css and js files
+# Step9: - configure settings.py file to add css to all our html.files
+# Step10: - adding css to our html page                              
+# Step11: - adding js to our html page                                 
+# Step12: - create app inside project using command py manage.py startapp { app name }  
+# Step13: - registering the app created in main project file ie. settings.py file                              
+# Step14: - create a urls.py file for app we create and we need to include this file in main project urls.py file                             
+# Step15:- base template for all our html pages using {% block (name) %} ....content...... {% endblock %} 
+# Step16:- to use templates in other html files use {% extends 'filename.html' %}  
+        
